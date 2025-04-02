3c4bb1c04a51e3d21b4823f4d3aae7e3692803c9
David Cauê <davidcaue8@gmail.com>
2025-04-02 19:38:28 -0300

    project

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { routes } from "./routes";
+import { appErrors } from "./errors/appErrors";
+import { sqliteConnection } from "./databases";
+import { runMigrations } from "./databases/migrations";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(routes);
+
+app.use(appErrors);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
+
+sqliteConnection()
+  .then(() => {
+    console.log("Database is connected!");
+  })
+  .catch((error) => {
+    console.log("Database ERROR - ", error);
+  });
+
+runMigrations();
---
+++ src/services/authServices.ts
@@ -0,0 +1,31 @@
+import { compare } from "bcrypt";
+import { AppError } from "../errors/appError";
+import { UserRepositoryTypes } from "./userServices";
+import { AuthDataTypes } from "../validations/authSchema";
+import { sign } from "jsonwebtoken";
+
+export const authServices = {
+  async login({ email, password }: AuthDataTypes, repository: UserRepositoryTypes) {
+    try {
+      const user = await repository.getUserByEmail(email);
+
+      if (!user) {
+        throw new AppError("email or password invalid", 401);
+      }
+
+      const passwordCheck = await compare(password, user.password);
+
+      if (!passwordCheck) {
+        throw new AppError("email or password invalid", 401);
+      }
+
+      const token = sign({ id: user.id }, process.env.SECRET_TOKEN, {
+        expiresIn: "1d",
+      });
+
+      return { id: user.id, token: token };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
